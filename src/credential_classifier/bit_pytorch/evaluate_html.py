452f4769dc4a38a30f1e0401b61390bd36a47694
ruofanliu <kelseyliu1988@gmail.com>
2021-10-09 14:58:46 +0800

    modify code on crp classifier evaluation

--- src/credential_classifier/bit_pytorch/evaluate_html.py
+++ src/credential_classifier/bit_pytorch/evaluate_html.py
@@ -16,7 +16,7 @@ def html_heuristic(html_path, obfuscate=False):
     proc_data = proc_tree(tree, obfuscate=obfuscate)
     return check_post(proc_data, version=2)
 
-def evaluate(model, train_loader, all_folder, obfuscate=False):
+def evaluate(model, train_loader, all_folder, obfuscate=False, method='cv_html'):
     '''
     :param model: model to be evaluated
     :param train_loader: dataloader to be evaluated
@@ -26,6 +26,8 @@ def evaluate(model, train_loader, all_folder, obfuscate=False):
     model.eval()
     correct = 0
     total = 0
+    predicted_results = []
+    gt_results = []
     with torch.no_grad():
         for b, (x, y, file) in tqdm(enumerate(train_loader)):
 
@@ -41,23 +43,35 @@ def evaluate(model, train_loader, all_folder, obfuscate=False):
                 preds = html_heuristic(html_path, obfuscate)
 
             ############ if use HTML heuristic only: HTML heuristic is correct
-            #             if preds == y.item():
-            #                 correct += 1
-            #                 print("\nGT:", y.item())
-            #                 print("Pred by HTML:", preds)
-            #                 print(correct, total)
+            if method == 'html':
+                if preds == y.item():
+                    correct += 1
+                    print("\nGT:", y.item())
+                    print("Pred by HTML:", preds)
+                    print(correct, total)
             #######################################################
 
             ############ if use HTML+CV ########################
+            elif method == 'cv_html':
+                if preds == 0 and preds == y.item():  # if HTML heuristic says it has post form
+                    correct += 1
+                    print("\nGT:", y.item())
+                    print("Pred by HTML:", preds)
+                    print(correct, total)
 
-            if preds == 0 and preds == y.item():  # if HTML heuristic says it has post form
-                correct += 1
-                print("\nGT:", y.item())
-                print("Pred by HTML:", preds)
-                print(correct, total)
+                else:  # inconclusive, use CRP classifier
+                    # Compute output, measure accuracy
+                    logits = model(x)
+                    preds = torch.argmax(logits, dim=1)
+                    correct += preds.eq(y).sum().item()
+                    print("\nGT:", y.item())
+                    print("Pred by classifier:", preds.item())
+                    print(correct, total)
+            ########################################################
 
-            else:  # inconclusive, use CRP classifier
-                # Compute output, measure accuracy
+            ############ if use CV only ########################
+            # Compute output, measure accuracy
+            elif method == 'cv':
                 logits = model(x)
                 preds = torch.argmax(logits, dim=1)
                 correct += preds.eq(y).sum().item()
@@ -66,17 +80,26 @@ def evaluate(model, train_loader, all_folder, obfuscate=False):
                 print(correct, total)
             ########################################################
 
-    return float(correct / total)
+            predicted_results.append(preds)
+            gt_results.append(y.item())
+
+    return float(correct / total), predicted_results, gt_results
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Evaluate trained model")
-    parser.add_argument("--weights_path", default='./src/credential_classifier/output/Increase_resolution_lr0.005/BiT-M-R50x1V2_0.005.pth.tar',
+    # parser.add_argument("--weights_path", default='./src/credential_classifier/output/Increase_resolution_lr0.005/BiT-M-R50x1V2_0.005.pth.tar',
+    #                         help="Where to load pretrained weights")
+    # parser.add_argument("--weights_path", default='./src/credential_classifier/output/screenshot/screenshot/BiT-M-R50x1_0.01.pth.tar',
+    #                     help="Where to load pretrained weights")
+    parser.add_argument("--weights_path", default='./src/credential_classifier/output/website_finetune/websiteV2/FCMaxV2_0.005.pth.tar',
                         help="Where to load pretrained weights")
     args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = models.KNOWN_MODELS['BiT-M-R50x1V2'](head_size=2)
+    # model = models.KNOWN_MODELS['BiT-M-R50x1V2'](head_size=2)
+    # model = models.KNOWN_MODELS['BiT-M-R50x1'](head_size=2)
+    model = models.KNOWN_MODELS['FCMaxV2'](head_size=2)
 
     # load weights
     checkpoint = torch.load(args.weights_path, map_location="cpu")
@@ -100,26 +123,35 @@ if __name__ == '__main__':
     test_img_folder = './datasets/val_merge_imgs'
     test_all_folder = './datasets/val_merge_folder'
 
-    train_set = HybridLoaderDebug(img_folder=train_img_folder,
-                          annot_path='./datasets/train_coords.txt')
+    # train_set = HybridLoaderDebug(img_folder=train_img_folder,
+    #                       annot_path='./datasets/train_coords.txt')
+    # #
+    # val_set = HybridLoaderDebug(img_folder=test_img_folder,
+    #                       annot_path='./datasets/val_merge_coords.txt')
 
-    val_set = HybridLoaderDebug(img_folder=test_img_folder,
-                          annot_path='./datasets/val_merge_coords.txt')
+    # train_set = ScreenshotLoaderDebug(img_folder=train_img_folder,
+    #                            annot_path='./datasets/train_coords.txt')
+    #
+    # val_set = ScreenshotLoaderDebug(img_folder=test_img_folder,
+    #                          annot_path='./datasets/val_merge_coords.txt')
 
-    # train_set = LayoutLoaderDebug(img_folder=train_img_folder,
-    #                            annot_path='/home/l/liny/ruofan/PhishIntention/datasets/train_coords.txt')
+    train_set = LayoutLoaderDebug(img_folder=train_img_folder,
+                               annot_path='./datasets/train_coords.txt')
 
-    # val_set = LayoutLoaderDebug(img_folder=test_img_folder,
-    #                          annot_path='/home/l/liny/ruofan/PhishIntention/datasets/val_merge_coords.txt')
+    val_set = LayoutLoaderDebug(img_folder=test_img_folder,
+                             annot_path='./datasets/val_merge_coords.txt')
 
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, drop_last=False, shuffle=False)
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, drop_last=False, shuffle=False)
 
-    # acc = evaluate(model, train_loader, train_all_folder, obfuscate=True)
+    # acc, predicted_results, gt_results = evaluate(model, train_loader, train_all_folder, obfuscate=False, method='cv_html')
     # print('Training Acc : {:.4f}'.format(acc))
     #
-    acc = evaluate(model, val_loader, test_all_folder, obfuscate=True)
-    print('Validation Acc : {:.4f}'.format(acc))
+    acc, predicted_results, gt_results = evaluate(model, val_loader, test_all_folder, obfuscate=False, method='cv_html')
+    print('Acc : {:.4f}'.format(acc))
+    print('Recall : {:.4f}'.format(np.sum((np.asarray(gt_results) == np.asarray(predicted_results)) & (np.asarray(gt_results) == 0)) / np.sum(np.asarray(gt_results) == 0) ))
+    print('Precision : {:.4f}'.format(np.sum((np.asarray(gt_results) == np.asarray(predicted_results)) & (np.asarray(gt_results) == 0)) / np.sum(np.asarray(predicted_results) == 0)))
+
 
 
 
