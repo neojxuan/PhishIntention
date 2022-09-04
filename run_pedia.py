bcb0c5fd8e7888427c6f8c869de57d1498ad820c
ruofanliu <kelseyliu1988@gmail.com>
2022-09-04 15:01:31 +0800

    support phishpedia

--- run_pedia.py
+++ run_pedia.py
@@ -1,5 +1,5 @@
 
-from phishintention.phishintention_main import *
+from phishintention.phishpedia_main import *
 import time
 import datetime
 import sys
@@ -21,13 +21,13 @@ if __name__ == '__main__':
         parser.add_argument('-f', "--folder",
                             default='E:\\screenshots_rf\\{}'.format(date),
                             help='Input folder path to parse')
-        parser.add_argument('-r', "--results", default=date + '.txt',
+        parser.add_argument('-r', "--results", default=date + '_pedia.txt',
                             help='Input results file name')
         parser.add_argument('--repeat', action='store_true')
         parser.add_argument('--no_repeat', action='store_true')
         args = parser.parse_args()
         print(args)
-        runit(args.folder, args.results, AWL_MODEL, CRP_CLASSIFIER, CRP_LOCATOR_MODEL, SIAMESE_MODEL, OCR_MODEL,
+        runit_pedia(args.folder, args.results, AWL_MODEL, CRP_CLASSIFIER, CRP_LOCATOR_MODEL, SIAMESE_MODEL, OCR_MODEL,
               SIAMESE_THRE, LOGO_FEATS, LOGO_FILES, DOMAIN_MAP_PATH) # if running phishintention
         print('Process finish')
 
